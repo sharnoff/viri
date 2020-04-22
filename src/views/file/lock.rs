d921c168528ec6d6aff20e07bc38cf8a89003086
sharnoff <github@max.sharnoff.org>
2020-04-22 11:12:58 +0100

    Add lock documentation, reduce retrictions on read locks

--- src/views/file/lock.rs
+++ src/views/file/lock.rs
@@ -1,12 +1,11 @@
 //! Utilities for a special lock used in the `ContentProvider` implementation for `Handle`
 //!
-//! The primary type defined here is [`Lock`], which provides many different access methods. The
+//! The primary type defined here is [`Lock`], which provides multiple different access methods. The
 //! locking type is not designed to be particularly efficient; this is an intentional, temporary
-//! choice to allow for better ergonomics.
+//! choice to allow for better ergonomics. For more information, see [the type's documentation].
 //!
-//! [`Lock`]: struct.Lock.hmtl
-// FIXME: Ease restrictions on acquiring read locks. Those could be acquired so long as `owner` is
-// `None`, and we would only set owner to Some(..) when we acquire a write lock.
+//! [`Lock`]: struct.Lock.html
+//! [the type's documentation]: struct.Lock.html
 
 use std::cell::UnsafeCell;
 use std::fmt::{self, Debug, Formatter};
@@ -20,29 +19,57 @@ pub type RwLock<T> = lock_api::RwLock<RawRwLock, T>;
 pub type RwLockReadGuard<'a, T> = lock_api::RwLockReadGuard<'a, RawRwLock, T>;
 pub type RwLockWriteGuard<'a, T> = lock_api::RwLockWriteGuard<'a, RawRwLock, T>;
 
+/// A reentrant, manually-lockable `RwLock`
+///
+/// What does the above actually mean? This type exposes a few different operations: [locking] (and
+/// [unlocking]), acquring [read locks], and acquiring [write locks]. The semantics are slightly
+/// different from the default `RwLock`; the type can be locked, which prevents other threads from
+/// acquiring write locks.
+///
+/// The precise meaning of reentrant in this context should be noted: **Write locks are not
+/// reentrant**. The lock is only reentrant in the sense that multiple calls to [`lock`] may be
+/// used in a reentrant fashion. When locked, only the thread holding the lock can acquire write
+/// lock, though other threads **may still acquire read locks**.
+///
+/// All calls to `lock` must be matched by an accompanying `unlock`. Failure to do so *will* result
+/// in deadlocks.
+///
+/// [locking]: #method.lock
+/// [unlocking]: #method.unlock
+/// [read locks]: #method.read
+/// [write locks]: #method.write
+/// [`lock`]: #method.lock
 pub struct Lock<T> {
+    /// The number
     count: UnsafeCell<usize>,
     owner: Mutex<Option<ThreadId>>,
     raw_mux: RawMutex,
     inner: RwLock<T>,
 }
 
-unsafe impl<T> Sync for Lock<T> {}
+// TODO: Are these correct? This is what is provided for `RwLock`.
+unsafe impl<T: Send> Send for Lock<T> {}
+unsafe impl<T: Send + Sync> Sync for Lock<T> {}
 
+/// A RAII structure around shared read access to the internal `RwLock`
+///
+/// This is created by the [`read`] method on [`Lock`].
+///
+/// [`read`]: struct.Lock.html#method.read
+/// [`Lock`]: struct.Lock.html
 pub struct ReadGuard<'a, T> {
-    lock: &'a Lock<T>,
     guard: RwLockReadGuard<'a, T>,
 }
 
+/// A RAII structure around unique write access to the internal `RwLock`
+///
+/// This is created by the [`write`] method on [`Lock`].
+///
+/// [`write`]: struct.Lock.html#method.write
+/// [`Lock`]: struct.Lock.html
 pub struct WriteGuard<'a, T> {
     lock: &'a Lock<T>,
     guard: RwLockWriteGuard<'a, T>,
-}
-
-impl<T> Drop for ReadGuard<'_, T> {
-    fn drop(&mut self) {
-        self.lock.unlock();
-    }
 }
 
 impl<T> Drop for WriteGuard<'_, T> {
@@ -52,6 +79,9 @@ impl<T> Drop for WriteGuard<'_, T> {
 }
 
 impl<T> Lock<T> {
+    /// Creates a new `Lock` with the given value
+    ///
+    /// The lock is initialized as unlocked.
     pub fn new(val: T) -> Self {
         Self {
             count: UnsafeCell::new(0),
@@ -61,6 +91,9 @@ impl<T> Lock<T> {
         }
     }
 
+    /// Locks the `Lock`
+    ///
+    /// For more information, refer to the type's documentation.
     pub fn lock(&self) {
         let id = thread::current().id();
         let mut owner_guard = self.owner.lock();
@@ -104,6 +137,9 @@ impl<T> Lock<T> {
         }
     }
 
+    /// Unlocks the `Lock`
+    ///
+    /// For more information, refer to the type's documentation.
     pub fn unlock(&self) {
         // decrement count
         let count = unsafe {
@@ -118,15 +154,18 @@ impl<T> Lock<T> {
         }
     }
 
+    /// Acquires a read lock on the underlying `RwLock`
+    ///
+    /// For more information, refer to the type's documentation.
     pub fn read(&self) -> ReadGuard<T> {
-        self.lock();
-
         ReadGuard {
-            lock: self,
             guard: self.inner.read(),
         }
     }
 
+    /// Acquires a write lock on the underlying `RwLock`
+    ///
+    /// For more information, refer to the type's documentation.
     pub fn write(&self) -> WriteGuard<T> {
         self.lock();
 
