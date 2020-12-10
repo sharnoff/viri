9bba956526b8923c6c35d923a76b88e8364f02eb
sharnoff <github@max.sharnoff.org>
2020-12-10 01:52:04 +0000

    add typed `NamedFunction`s

--- src/config/named_fn.rs
+++ src/config/named_fn.rs
@@ -11,6 +11,7 @@ use std::any::Any;
 use std::collections::HashMap;
 use std::convert::TryFrom;
 use std::fmt::{self, Debug, Display, Formatter};
+use std::marker::PhantomData;
 use std::sync::Arc;
 
 fn collect_funcs() -> HashMap<&'static str, &'static RegisteredFunction> {
@@ -171,6 +172,173 @@ pub enum BadInput {
         given: Type,
         func: &'static RegisteredFunction,
     },
+}
+
+/// A [`NamedFunction`] with guarantees about the input/output types
+///
+/// This allows building configurations with [`NamedFunction`]s where the input and output types of
+/// that function are known.
+///
+/// Usage of this type is typically much more ergonomic than [`NamedFunction`]; the version of the
+/// `apply` method given here is statically-typed.
+///
+/// ## Example
+///
+/// Suppose we need to use a predicate on integers - i.e. a function from `i32`s to `bool`s. This
+/// might be represented as:
+/// ```
+/// #[derive(serde::Deserialize)]
+/// struct Foo {
+///     predicate: TypedNamedFunction<dyn Fn(i32) -> bool>,
+/// }
+/// ```
+/// In order to give a concrete type, the type argument for a `TypedNamedFunction` must be a
+/// `dyn Fn(_) -> _` (with a variable number of arguments). The choice of `Fn` instead of `FnOnce`
+/// or `FnMut` is purely stylistic; it makes no bearing on the actual ownership requirements to
+/// execute the function.
+#[derive(Copy, Clone, Debug, Serialize, Deserialize)]
+#[serde(try_from = "NamedFunction")]
+pub struct TypedNamedFunction<F: FuncType> {
+    #[serde(flatten)]
+    func: NamedFunction,
+    #[serde(skip)]
+    _marker: PhantomData<F>,
+}
+
+impl<F: FuncType> TypedNamedFunction<F> {
+    /// The typed equivalent of [`NamedFunction::apply`]
+    pub async fn apply(&self, inputs: F::Inputs) -> F::Output {
+        let output = self.func.apply(F::inputs_to_dyn_vec(inputs)).await;
+        *<Box<dyn Any + Send>>::downcast(output)
+            .unwrap_or_else(|_| panic!("unexpected output type from typed named function"))
+    }
+}
+
+////////////////////
+// trait FuncType //
+////////////////////
+
+/// A marker trait for `dyn Fn(X0,X1,..XN) -> Y`
+///
+/// This trait is only used in the bound on [`TypedNamedFunction`], and implementations are
+/// auto-generated for functions with up to 12 arguments. This trait is not intended for use
+/// outside of the implementation of [`TypedNamedFunction`].
+pub trait FuncType {
+    /// The types expected as input to this function
+    ///
+    /// Ideally, this would have directly been a constant or static value, but limitations in the
+    /// type system prevent this from happening. We *also* can't simply shift the location of a
+    /// static value to return a reference here (like `&'static [Type]`), so we have to resort to
+    /// constructing the list at "runtime" (primarily just allocation) instead.
+    ///
+    /// This isn't ideal, but it's a small, internal trait - it shouldn't really matter.
+    fn input_types() -> Vec<Type>;
+
+    /// The representation of the input types as a tuple, *within* the type system
+    type Inputs: Any;
+
+    /// The type of the returned value from this function
+    const OUTPUT_TYPE: Type = Type::new::<Self::Output>();
+
+    /// The representation of the output type of the function, within the type system
+    type Output: Any;
+
+    /// A helper function to convert to a list of inputs that can be supplied to
+    /// [`NamedFunction::apply`]
+    fn inputs_to_dyn_vec(inp: Self::Inputs) -> Vec<Box<dyn Any + Send + Sync>>;
+}
+
+macro_rules! impl_FuncType {
+    ($out:ident; $head:ident $idx:tt $(, $tail:ident $tail_idx:tt)*) => {
+        impl<$out: Any, $head $(, $tail)*> FuncType for dyn Fn($head $(, $tail)*) -> $out
+        where
+            $head: Any + Send + Sync,
+            $($tail: Any + Send + Sync,)*
+        {
+            fn input_types() -> Vec<Type> {
+                vec![Type::new::<$head>() $(, Type::new::<$tail>() )*]
+            }
+
+            type Inputs = ($head, $($tail,)*);
+
+            type Output = $out;
+
+            fn inputs_to_dyn_vec(inputs: Self::Inputs) -> Vec<Box<dyn Any + Send + Sync>> {
+                let mut inps = <Vec<Box<dyn Any + Send + Sync>>>::new();
+                inps.push(Box::new(inputs.$idx));
+                $(inps.push(Box::new(inputs.$tail_idx));)*
+                inps.reverse();
+                inps
+            }
+        }
+    };
+    ($out:ident;) => {
+        impl<$out: Any> FuncType for dyn Fn($head $(, $tail)*) -> $out {
+            fn input_types() -> Vec<Type> { Vec::new() }
+
+            type Inputs = ();
+
+            type Output = $out;
+
+            fn inputs_to_dyn_vec(inp: ()) -> Vec<Box<dyn Any + Send + Sync>> {
+                Vec::new()
+            }
+        }
+    };
+}
+
+impl_FuncType!(Z; L 11, K 10, J 9, I 8, H 7, G 6, F 5, E 4, D 3, C 2, B 1, A 0);
+
+impl<F: FuncType> From<TypedNamedFunction<F>> for NamedFunction {
+    fn from(typed: TypedNamedFunction<F>) -> NamedFunction {
+        typed.func
+    }
+}
+
+impl<F: FuncType> TryFrom<NamedFunction> for TypedNamedFunction<F> {
+    type Error = String;
+
+    fn try_from(func: NamedFunction) -> Result<Self, String> {
+        let input_types = F::input_types();
+
+        // First, check the number of inputs:
+        if input_types.len() != func.0.input.len() {
+            return Err(format!(
+                "function {:?} has wrong number of arguments; expected `{}` but has `{}`",
+                func.name(),
+                input_types.len(),
+                func.0.input.len(),
+            ));
+        }
+
+        // Then, check each input individually:
+        for (idx, ty) in input_types.into_iter().enumerate() {
+            if func.0.input[idx] != ty {
+                return Err(format!(
+                    "type mismatch: argument {} of function '{}' is required to be `{}`, but is actually `{}`",
+                    idx,
+                    func.name(),
+                    func.0.input[idx].name(),
+                    ty.name(),
+                ));
+            }
+        }
+
+        // And finally, check the output type:
+        if func.output_type() != F::OUTPUT_TYPE {
+            return Err(format!(
+                "type mismatch: output function '{}' is required to be `{}`, but is actually `{}`",
+                func.name(),
+                F::OUTPUT_TYPE.name(),
+                func.output_type().name(),
+            ));
+        }
+
+        Ok(TypedNamedFunction {
+            func,
+            _marker: PhantomData,
+        })
+    }
 }
 
 ///////////////////////////////////
